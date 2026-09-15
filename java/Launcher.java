f2685daae51363a6b18645a3f1428429e2c20e8c
Craig <craighulme23@gmail.com>
2026-09-15 22:51:05 +0100

    Register the bundled Swing font

--- java/Launcher.java
+++ java/Launcher.java
@@ -1,5 +1,7 @@
 package dev.quackduck;
 
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.io.PrintWriter;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
@@ -17,6 +19,9 @@ public final class Launcher {
 
         try {
             Class.forName("com.github.caciocavallosilano.cacio.ctc.CTCPreloadClassLoader");
+            Path font = Path.of(System.getProperty("java.home"), "lib", "fonts", "NotoSans.ttf");
+            GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .registerFont(Font.createFont(Font.TRUETYPE_FONT, font.toFile()));
             Files.writeString(status, Files.readString(status) + "AWT_BRIDGE_OK\n");
             Class<?> runelite = Class.forName("net.runelite.client.RuneLite");
             Files.writeString(status, Files.readString(status) + "RUNITELITE_CLASS_OK\n");
